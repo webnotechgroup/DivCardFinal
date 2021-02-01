eafae1329f7baff82ef603f797afdc34c24fb852
Reza Khani <m.r.khani.69@gmail.com>
2021-02-02 00:17:37 +0330

    Add ViewComponents for latestarticles & projects in Index

---
+++ DivCardFinal/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DivCardFinal.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("_Projects");
+        }
+    }
+}
