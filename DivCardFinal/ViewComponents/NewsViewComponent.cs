14aae6ac89a35a9d01e2dfd5cf87d4ee20de6d18
Reza Khani <m.r.khani.69@gmail.com>
2021-02-07 01:09:28 +0330

    اضافه شدن هدر و فوتر به پارشیال ویو های خودشون

---
+++ DivCardFinal/ViewComponents/NewsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DivCardFinal.ViewComponents
+{
+    public class NewsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("_News");
+        }
+    }
+
+}
